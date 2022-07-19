4fba7c6684cc053b0f029e6fafcd458c3205e949
James Macapagal <j.macapagal@gmail.com>
2022-07-19 10:11:56 -0500

    commit tree 040ff45fb9c3743cd938723e25c9fe096eb74d50

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -6,6 +6,12 @@ class BirdsController < ApplicationController
     render json: birds
   end
 
+  def create
+    
+    bird = Bird.create(name: params[:name] , species: params[:species])
+    render json: bird, status: :created
+  end
+
   # GET /birds/:id
   def show
     bird = Bird.find_by(id: params[:id])
